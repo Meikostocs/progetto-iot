b154f3779cdcd921888e280eb6335b2f3dd794e9
CWRLD02 <cirillocristianpio02@gmail.com>
2024-01-29 13:20:47 +0100

    Added mqtt configuration and breathing monitor models On /conf the file is [MAT_number]_mqtt_conf.py. Added also my models [brathing_monitor_telemetry, breathing_monitor_descriptor] on /models

---
+++ project-bin/model/breathing_monitor_descriptor.py
@@ -0,0 +1,20 @@
+import json
+
+class BreathingMonitorDescriptor:
+    
+    
+    def __init__(self, id_room, id_bed):
+        """
+        Describes the data for the Breathing Monitor. 
+        The monitor is uniquely identified by the tuple (id_room, id_bed), where id_room represents the room identifier and id_bed represents the bed identifier.
+        :param id_room: Room id.
+        :type id_room: str
+        :param id_bed: Bed id.
+        :type id_bed: str
+        """
+        self.id_room = id_room
+        self.id_bed  = id_bed
+
+        
+    def to_json(self):
+        return json.dump(self,default=lambda o: o.__dict__)
---
+++ project-bin/model/breathing_monitor_telemetry_data.py
@@ -0,0 +1,24 @@
+class BreathingMonitorTelemtryData:
+    def __init__(self, spo2, resp, co2, etco2):
+        """
+        Caputure data.
+        
+        :param spo2: SpO2 (Blood oxygen saturation value).
+        :type spo2: float [%]
+        :param resp: RESP, Respiration Frequency.
+        :type resp: int [BPM]
+        :param co2: CO2 value.
+        :type co2: float [mmHg]
+        :param etco2: End-Tidal CO2. It measures the concentration of CO2 at exhalation end. 
+        :type etco2: dict
+            - "pressure":"mmHg".
+            - "percentage":"%".
+        """
+        self.spo2  = spo2
+        self.resp  = resp
+        self.co2   = co2
+        self.etco2 = etco2
+
+
+    def to_json(self):
+        return json.dump(self,default=lambda o: o.__dict__)
